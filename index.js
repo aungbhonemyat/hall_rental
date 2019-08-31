195f3fb026fd7e0ccd37f16809fcd6e8799ee15e
aungbhonemyat <54734443+aungbhonemyat@users.noreply.github.com>
2019-08-31 07:26:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
+app.get('/', (req, res)=>{
+	res.send("Hello vro!");
+})
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
